81b09d4f73c25ce092ce45501ea4f76e7e6695f1
ZanderOl <olexandruk@gmail.com>
2023-01-19 02:57:54 +0200

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -11,7 +11,7 @@ const StyledLink = styled(NavLink)`
   }
 
   &.active {
-    color: orange;
+    color: lightslategrey;
   }
 
   :hover:not(.active),
@@ -33,7 +33,7 @@ const Header = styled.header`
   padding-bottom: 12px;
   margin-bottom: 10px;
   color: #fff;
-  background-color: #28718d;
+  background-color: #30d5c8;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
